3953a007c2ee44eaca8126426928d8c8bd46cd8c
Dylan <agnello.dylan95@gmail.com>
2023-04-29 10:49:17 -0600

    adding all the models still need to figure out matching the email validation and getter method to format the timestamp on query

---
+++ models/Thought.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types } = require('mongoose');
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 444,
+            minlength: 1
+
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+
+        },
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    }
+)
+ThoughtSchema
+    .virtual('reactionCount')
+    // Getter
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
